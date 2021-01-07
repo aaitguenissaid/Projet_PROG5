e0539ead05f110742f895268eb06d92087a18fa9
Josquin IMBERT <josquin.imbert@etu.univ-grenoble-alpes.fr>
2021-01-07 09:52:45 +0100

    Début Spécification Instructions Branchement

--- arm_branch_other.h
+++ arm_branch_other.h
@@ -25,8 +25,40 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdint.h>
 #include "arm_core.h"
 
+/*
+   arm_branch
+   description : effectue le branchement décrit par l'instruction ins dans le coeur p.
+		 Dans la documentation ARMv5, regarder :
+			- A4.1.5 - B, BL (page A4-10)
+			- A4.1.8 - BLX(1) et A4.1.9 - BLX(2) (pages A4-16 et A4-18)
+			- A4.1.10 - BX (page A4-20)
+   parametres : Le coeur arm_core appelé p, l'instruction sur 32 bits.
+   valeur de retour : //TODO si l'instruction a bien été exécutée, UNDEFINED_INSTRUCTION si elle n'a pas été reconnue. (cf arm_constants.h pour les valeurs)
+   effets de bord : p->registers est modifié (peut modifier PC, CPSR, SPSR, LR et des registres généraux)
+*/
 int arm_branch(arm_core p, uint32_t ins);
+
+/*
+   arm_coprocessor_others_swi
+   description : exécute les instructions SWI et autres.
+		 Dans la documentation ARMv5, regarder :
+			- A3.14.1 - Instruction encodings (page A3-29)
+   parametres : Le coeur arm_core appelé p, l'instruction sur 32 bits.
+   valeur de retour : SOFTWARE_INTERRUPT si une instruction SWI a été exécutée, //TODO si une autre instruction a été exécutée, UNDEFINED_INSTRUCTION sinon. (cf arm_constants.h pour les valeurs)
+   effets de bord :
+*/
 int arm_coprocessor_others_swi(arm_core p, uint32_t ins);
-int arm_miscellaneous(arm_core p, uint32_t ins);
+
+/*
+   arm_miscellaneous
+   description : exécute des instructions ins diverses dans le coeur p.
+		 Dans la documentation ARMv5, regarder :
+		 	- A3.8 - Miscellaneous arithmetic instructions (page A3-17)
+		 	- A3.9 - Other miscellaneous instructions (page A3-18)
+   parametres : Le coeur arm_core appelé p, l'instruction sur 32 bits.
+   valeur de retour : //TODO si l'instruction a bien été exécutée, UNDEFINED_INSTRUCTION si elle n'a pas été reconnue. (cf arm_constants.h pour les valeurs)
+   effets de bord : Modification du coeur p (lecture / écriture de registres)
+*/
+int arm_miscellaneous(arm_core p, uint32_t ins); //miscellaneous = divers
 
 #endif
