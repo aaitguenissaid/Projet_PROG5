885bfcd134a9f1624c5735a0f40f61c80e5a27c6
jizong2020 <jizongzhan@gmail.com>
2021-01-07 15:51:02 +0100

    changer Rm Rn pour éviter le conflit entre fonction!

--- arm_load_store.c
+++ arm_load_store.c
@@ -29,35 +29,35 @@ typedef enum NomsDeFunc { LDR, LDRB, LDRH, STR, STRB, STRH, LDM, STM}
 
 
 uint32_t get_index(uint32_t ins){
-	uint32_t Rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
+	uint32_t rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
 	uint8_t shift_imm = get_bits(ins, 11, 7);
 	uint8_t shift_ind = get_bits(ins, 6, 5);
 	uint32_t index;
 	switch (shift_ind)
 	    {
 		case 0:
-			index = Rm << shift_imm;
+			index = rm << shift_imm;
 			break;
 		case 1:
 			if (shift_imm == 0)
 			    index = 0;
 			else
-			    index = Rm >> shift_imm;
+			    index = rm >> shift_imm;
 				break;
 	    case 2:
 			{
 			if (shift_imm == 0){
-			    if (get_bit(Rm, 31) == 1){
+			    if (get_bit(rm, 31) == 1){
 					index = 0xFFFFFFFF;
 				} else{
 					index = 0; 
 				}						
 			} else {
-			    if (Rm >= 0){
-					index = Rm >> shift_imm;
+			    if (rm >= 0){
+					index = rm >> shift_imm;
 				} else {   
 					uint32_t masque = ((0xFFFFFFFF >> (32-shift_imm)) << (32-shift_imm));
-			        index = (Rm >> shift_imm) | masque;
+			        index = (rm >> shift_imm) | masque;
 			        }
 			    }
 			    break;
@@ -67,9 +67,9 @@ uint32_t get_index(uint32_t ins){
 			if (shift_imm == 0){
 				uint8_t cpsr = arm_read_cpsr(p);
 				uint32_t c = get_bit(cpsr,29);
-				index = c | (Rm << 1);
+				index = c | (rm << 1);
 			} else {
-				index = (Rm << shift_imm) | (Rm >> (32-shift_imm));
+				index = (rm << shift_imm) | (rm >> (32-shift_imm));
 				}					
 			break;
 	        }
@@ -99,11 +99,11 @@ void condtionPassFonc_modifieRn(arm_core p, uint32_t ins, uint32_t op_droit){
 		if ConditionPassed(p, ins){
 			uint32_t Rn_write;
 			int u = get_bit(ins, 23);
-			uint32_t Rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
+			uint32_t rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
 			if(u){
-	        	Rn_write = Rn + op_droit;
+	        	Rn_write = rn + op_droit;
 	    	}else {
-	        	Rn_write = Rn - op_droit;
+	        	Rn_write = rn - op_droit;
 	    	}
 			arm_write_register(p,(uint8_t) get_bits(ins, 19, 16), Rn_write);	
 		}
@@ -111,38 +111,38 @@ void condtionPassFonc_modifieRn(arm_core p, uint32_t ins, uint32_t op_droit){
 
 uint32_t get_adres(arm_core p, uint32_t ins){
 	int u = get_bit(ins, 23);
-	uint32_t Rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
-	uint32_t Rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
+	uint32_t rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
+	uint32_t rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
 	uint32_t offset_12 = get_bits(ins, 11, 0);
 	uint8_t bits_2724 = get_bits(ins,27, 24);
 	uint8_t bits_21 = get_bit(ins, 21);
 	uint32_t adresse = 0;
 	if ((bits_2724 == 0x5) && (bits_21 == 0)){
 		// 1.Load and Store Word or Unsigned Byte - Immediate offset
-		operation_1(u, &adresse, Rn, offset_12);
+		operation_1(u, &adresse, rn, offset_12);
 		return adresse;
 	}
 	else if ((bits_2724==0x07) && (bits_21 == 0) && (get_bits(ins,11,4) == 0)){
 		// 2.Load and Store Word or Unsigned Byte - Register offset
-		operation_1(u, &adresse, Rn, Rm);
+		operation_1(u, &adresse, rn, rm);
 		return adresse;
 	}
 	else if ((bits_2724 == 0x7) && (bits_21 == 0) && (get_bit(ins, 4) == 0)){
    		// 3.Load and Store Word or Unsigned Byte - Scaled register offset
 		uint32_t index = get_index(ins);
-		operation_1(u, &adresse, Rn, index);
+		operation_1(u, &adresse, rn, index);
 		return adresse;
 			
 	}
 	else if ((bits_2724 == 0x05) && (bits_21 == 1)){
 		// 4.Load and Store Word or Unsigned Byte - Immediate pre-indexed
-		operation_1(u, &adresse, Rn, offset_12);
+		operation_1(u, &adresse, rn, offset_12);
 		condtionPassFonc(p, ins, &adresse);
 		return address;
 	}
 	else if ((bits_2724 == 0x07) && (bits_21 == 1) && (get_bits(ins,11,4) == 0)){
 		// 5.Load and Store Word or Unsigned Byte - Register pre-indexed
-		operation_1(u, &adresse, Rn, Rm);
+		operation_1(u, &adresse, rn, rm);
 		condtionPassFonc(p, ins, &adresse);
 		return address;
 	}
@@ -150,25 +150,25 @@ uint32_t get_adres(arm_core p, uint32_t ins){
 		// 6.Load and Store Word or Unsigned Byte - Scaled register pre-indexed
 
 	    uint32_t index = get_index(ins);
-		operation_1(u, &adresse, Rn, index);
+		operation_1(u, &adresse, rn, index);
 		condtionPassFonc(p, ins, &adresse);
 		return address;		
 	}
 	else if ((bits_2724 == 0x04) && (bits_21 == 0)){
 		// 7.Load and Store Word or Unsigned Byte - Immediate post-indexed
-		address = Rn;
+		address = rn;
 		condtionPassFonc_modifieRn(p, ins, offset_12)
 		return address;
 	} 
 	else if ((bits_2724 == 0x06) && (bits_21 == 0) && (get_bits(ins,11,4) == 0)){
 		// 8.Load and Store Word or Unsigned Byte - Register post-indexed
-		address = Rn;
-		condtionPassFonc_modifieRn(p, ins, Rm);
+		address = rn;
+		condtionPassFonc_modifieRn(p, ins, rm);
 		return address;
 	}
 	else if ((bits_2724 == 0x06) && (bits_21 == 0) && !get_bit(ins,4)){
 		// 9.Load and Store Word or Unsigned Byte - Scaled register post-indexed
-		address = Rn;
+		address = rn;
 	    uint32_t index = get_index(ins);
 		condtionPassFonc_modifieRn(p, ins, index);
 		return address;	
@@ -184,48 +184,48 @@ uint32_t get_offset(uint32_t ins){
 
 uint32_t get_adres_h(arm_core p,uint32_t ins){
 	int u = get_bit(ins, 23);
-	uint32_t Rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
+	uint32_t rn = arm_read_register(p, (uint8_t) get_bits(ins, 19, 16));
 	uint8_t bits_2724 = get_bits(ins, 27, 24);
 	uint8_t bits_2221 = get_bits(ins, 22, 21);
 	uint32_t adresse = 0;
 	if ((bits_2724 == 1) && (bits_2221 == 2)){
 		// 1.Miscellaneous Loads and Stores - Immediate offset
 		uint32_t offset_8 = get_offset(ins);
-		operation_1(u, &adresse, Rn, offset_8);
+		operation_1(u, &adresse, rn, offset_8);
 		return address;			
 	}
 	else if ((bits_2724 == 1) && (bits_2221 == 0))
 	{  // 2.Miscellaneous Loads and Stores - Register offset
-		uint32_t Rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
-		operation_1(u, &adresse, Rn, Rm);
+		uint32_t rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
+		operation_1(u, &adresse, rn, rm);
 		return address;	
 	}
 	else if ((bits_2724 == 1) && (bits_2221 == 3))
 	{  // 3.Miscellaneous Loads and Stores - Immediate pre-indexed
 		uint32_t offset_8 = get_offset(ins);
-		operation_1(u, &adresse, Rn, offset_8);
+		operation_1(u, &adresse, rn, offset_8);
 		condtionPassFonc(p, ins, &adresse);
 		return address;				
 	}
 	else if ((bits_2724 == 1) && (bits_2221 == 1))
 	{  // 4.Miscellaneous Loads and Stores - Immediate pre-indexed
-		uint32_t Rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
-		operation_1(u, &adresse, Rn, Rm);
+		uint32_t rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
+		operation_1(u, &adresse, rn, rm);
 		condtionPassFonc(p, ins, &adresse);
 		return address;				
 	}
 	else if ((bits_2724 == 0) && (bits_2221 == 2))
 	{  // 5.Miscellaneous Loads and Stores - Immediate post-indexed
-		address = Rn;
+		address = rn;
 		uint32_t offset_8 = get_offset(ins);
 		condtionPassFonc_modifieRn(p, ins, offset_8);
 		return address;				
 	}
 	else if ((bits_2724 == 0) && (bits_2221 == 0))
 	{  // 6.Miscellaneous Loads and Stores - Register post-indexed
-		address = Rn;
-		uint32_t Rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
-		condtionPassFonc_modifieRn(p, ins, Rm);
+		address = rn;
+		uint32_t rm = arm_read_register(p, (uint8_t) get_bits(ins, 3, 0));
+		condtionPassFonc_modifieRn(p, ins, rm);
 		return address;				
 	}
 }
