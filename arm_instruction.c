f57d374adab81510c5b97cba25aabb46a47445b2
aaitguenissaid <abderrahmane.ait-gueni-ssaid@etu.univ-grenoble-alpes.fr>
2021-01-07 10:01:49 +0100

    Des commentaires et correction indentation du arm_core.c

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -28,12 +28,34 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_constants.h"
 #include "util.h"
 
+/*
+La fonction arm_execute_instruction est la toute premiÃ¨re partie de la simulation
+dâ€™un cycle dâ€™exÃ©cution dâ€™une instruction dans le processeur.
+cette fonction devra contenir la partie commune Ã  lâ€™exÃ©cution de
+toute instruction ARM :
+la lecture de la prochaine instruction,
+lâ€™incrÃ©mentation du compteur ordinal
+lâ€™interprÃ©tation du champ dâ€™exÃ©cution conditionnelle.
+
+Ensuite, cette fonction devra sÃ©lectionner la classe dâ€™instructions concernÃ©e par lâ€™instruction courante et
+appeler la fonction appropriÃ©e, chargÃ©e de la suite du dÃ©codage.
+
+Lâ€™interprÃ©tation du champ dâ€™exÃ©cution conditionnelle et la dÃ©termination de la classe de lâ€™instruction
+courante peuvent Ãªtre implÃ©mentÃ©es principalement selon deux approches :
+bloc switch ou table depo inteurs de fonctions.
+
+Les listings (de pseudo-code) donnÃ©s en Figure 1 illustrent ces deux techniques
+de dÃ©codage, en considÃ©rant un champ de 3 bits extrait des bits 15 Ã  17 dâ€™un mot de 32 bits.
+*/
 static int arm_execute_instruction(arm_core p) {
     return 0;
 }
 
 int arm_step(arm_core p) {
     int result;
+    //fetch
+    //decode
+    //execute
 
     result = arm_execute_instruction(p);
     if (result)
