cdd03478a16149773b483efde98fc148f6b6402a
aaitguenissaid <abderrahmane.ait-gueni-ssaid@etu.univ-grenoble-alpes.fr>
2021-01-11 00:28:23 +0100

    fonction arm_execute_instructions maque quelques definitions dans le switch

--- arm_instruction.c
+++ arm_instruction.c
@@ -35,83 +35,102 @@ cette fonction devra contenir la partie commune à l’exécution de
 toute instruction ARM :
 DONE  la lecture de la prochaine instruction,
 DONE  l’incrémentation du compteur ordinal
-      l’interprétation du champ d’exécution conditionnelle.
+DONE  l’interprétation du champ d’exécution conditionnelle.
 
-      Ensuite, cette fonction devra sélectionner la classe d’instructions
+DONE  Ensuite, cette fonction devra sélectionner la classe d’instructions
       concernée par l’instruction courante et
       appeler la fonction appropriée, chargée de la suite du décodage.
 
-      L’interprétation du champ d’exécution conditionnelle et la détermination
-      de la classe de l’instructio courante peuvent être implémentées principalement
-      selon deux approches :
-      bloc switch ou table depo inteurs de fonctions.
-
-      Les listings (de pseudo-code) donnés en Figure 1 illustrent ces deux techniques
-      de décodage, en considérant un champ de 3 bits extrait des bits 15 à 17 d’un mot de 32 bits.
 */
 static int arm_execute_instruction(arm_core p) {
-    return 0;
+
     //fetch
-    uint32_t *accumulator;
-    int interuption = arm_fetch(p, *accumulator);
-
-    // par convention R0 est considéré comme l'accumulator
-    arm_write_usr_register(p,0,accumulator);
-    //decode 31 - 28
-    //je lis opcode de accumulator la variable ou le registre R0 ?
-    uint8_t opcode = (uint8_t) get_bits(accumulator, 31, 28);
-    switch(opcode){
-      case 0x00:  // 0000 AND Logical AND Rd := Rn AND shifter_operand
-
-      break;
-      case 0x01:  // 0001 EOR Logical Exclusive OR Rd := Rn EOR shifter_operand
-
-      break;
-      case 0x01:  // 0010 SUB Subtract Rd := Rn - shifter_operand
-
-      break;
-      case 0x03:  // 0011 RSB Reverse Subtract Rd := shifter_operand - Rn
-
-      break;
-      case 0x04:  // 0100 ADD Add Rd := Rn + shifter_operand
-
-      break;
-      case 0x05:  // 0101 ADC Add with Carry Rd := Rn + shifter_operand + Carry Flag
-
-      break;
-      case 0x06:  // 0110 SBC Subtract with Carry Rd := Rn - shifter_operand - NOT(Carry Flag)
-
-      break;
-      case 0x07:  // 0111 RSC Reverse Subtract with Carry Rd := shifter_operand - Rn - NOT(Carry Flag)
-
-      break;
-      case 0x08:  // 1000 TST Test Update flags after Rn AND shifter_operand
-
-      break;
-      case 0x09:  // 1001 TEQ Test Equivalence Update flags after Rn EOR shifter_operand
-
-      break;
-      case 0x0A:  // 1010 CMP Compare Update flags after Rn - shifter_operand
-
-      break;
-      case 0x0B:  // 1011 CMN Compare Negated Update flags after Rn + shifter_operand
-
-      break;
-      case 0x0C:  // 1100 ORR Logical (inclusive) OR Rd := Rn OR shifter_operand
-
-      break;
-      case 0x0D:  // 1101 MOV Move Rd := shifter_operand (no first operand)
-
-      break;
-      case 0x0E:  // 1110 BIC Bit Clear Rd := Rn AND NOT(shifter_operand)
-
-      break;
-      case 0x0F:  // 1111 MVN Move Not Rd := NOT shifter_operand (no first operand)
-
-      break;
-      default: // cas impossible
+    // ri Registre d'instruction (Variable local)
+    uint32_t *ri=NULL;
+    int exception = arm_fetch(p, *ri);
+    if(exception != 0){
+      return PREFETCH_ABORT;
     }
+    //decode 27 - 25
+     uint8_t opcode = (uint8_t) get_bits(*ri, 27, 25);
 
+    if(condition_passed(p, ri)){
+      switch(opcode){
+        case 0x00:
+        if(get_bit(ri, 4)==0)) {  // bit_4 = 0
+            if(get_bits(ri, 24, 23)==2 && get_bit(ri, 20)==0)){ //  Miscellaneous instructions
+              return arm_miscellaneous(p, ins);
+            } else { //  Data processing immediate shift
+              return arm_data_processing_immediate_msr(p, ins);
+            }
+        } else { // bit_4 = 1
+            if(get_bit(ri, 7)==0)){
+                if(get_bits(ri, 24, 23)==2 && get_bit(ri, 20)==1){  //  Miscellaneous instructions
+                  return arm_miscellaneous(p, ins);
+                } else { // Data processing register shift [2]
+
+                }
+            } else { // bit_7 = 1 Multiplies: See Figure A3-3
+                    //Extra load/stores: See Figure A3-5
+//############ ??????????
+            }
+        }
+        break;
+        case 0x01:
+        if(get_bits(ri, 24, 23)==2) {
+          if(get_bits(ri, 22, 21)==2) { // Move immediate to status register
+
+          } else { // Undefined instruction
+            return UNDEFINED_INSTRUCTION;
+          }
+        } else { // Data processing immediate [2]
+//############ ??????????
+        }
+        break;
+        // Load/store immediate offset
+        case 0x02:
+          return arm_load_store(p, ins);
+        break;
+        case 0x03:
+        if(get_bit(ri, 4)==0)) {  //  Load/store register offset
+          return arm_load_store(p, ins);
+        } else {
+          if(get_bits(ri, 24, 20)==0x1F && if(get_bits(ri, 7, 5)==0x07)){ // Architecturally undefined
+            return UNDEFINED_INSTRUCTION;
+          } else { // Media instructions [4]: See Figure A3-2
+//############ ??????????
+          }
+        }
+        break;
+        //Load/store multiple
+        case 0x04:
+          return arm_load_store_multiple(p, ins);
+        break;
+        //Branch and branch with link
+        case 0x05:
+          arm_branch(p, ins);
+        break;
+        //Coprocessor load/store and double register transfers
+        case 0x06:
+          return arm_load_store(arm_core p, uint32_t ins);
+        break;
+        case 0x07:
+        if(get_bit(ri,24)==0){
+          if(get_bit(ri,4)==0){ // Coprocessor data processing
+//############ ??????????
+          } else { // Coprocessor register transfers
+//############ ??????????
+          }
+        } else { // Software interrupt
+          return UNDEFINED_INSTRUCTION;
+        }
+        break;
+        default:
+        return UNDEFINED_INSTRUCTION;
+        break;
+      }
+  }
+return 0;
 }
 
 
