775b3745f9e157bbf4dce6a03d2677d63547ea2c
jizong2020 <jizongzhan@gmail.com>
2021-01-11 22:28:03 +0100

    fonction sur bl

--- arm_branch_other.c
+++ arm_branch_other.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_branch_other.h"
 #include "arm_constants.h"
@@ -27,14 +27,19 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 #define SUCCESS 0
-
-static void Branch(arm_core p, uint32_t branch_addr);
 static void Link(arm_core p);
+
+uint32_t sign_extend_30(int32_t value) {
+	return asr(value << 8, 8);
+}
+/*
+static void Branch(arm_core p, uint32_t branch_addr);
+
 static void Exchange(arm_core p);
 static uint32_t SignExtend(uint32_t val);
-
+*/
 int arm_branch(arm_core p, uint32_t ins) {
-	uint32_t branch_addr = 0x0;
+/*	uint32_t branch_addr = 0x0;
 	int defined = 0;
 	if(get_bits(ins, 27, 25) == 0x5) { //B, BL ou BLX
 		branch_addr = get_bits(ins, 23, 0);
@@ -54,8 +59,8 @@ int arm_branch(arm_core p, uint32_t ins) {
 		uint32_t cpsr_value = arm_read_register(p, CPSR);
 		uint8_t J = get_bit(cpsr_value,24), E = get_bit(cpsr_value,9);
 		branch_addr = Rm_value & 0xFFFFFFFE;
-		if(br==3 || br==1 || (br==2 && ( (!J && !E) /*|| (TODO)*/ ) ) ) {
-			Exchange(p);
+		if(br==3 || br==1 || (br==2 && ( (!J && !E)  ) ) ) {
+		Exchange(p);
 		}
 		if(br==3) {
 			Link(p);
@@ -65,7 +70,15 @@ int arm_branch(arm_core p, uint32_t ins) {
 		Branch(p, branch_addr);
 		return SUCCESS;
 	}
-	return UNDEFINED_INSTRUCTION;
+*/  
+	uint32_t pc_value = arm_read_register(p, PC);
+	if(get_bit(ins, 24)){
+		arm_write_register(p, LR, pc_value-4);
+	}
+	uint32_t signed_immed_24 = get_bits(ins, 23, 0);
+	uint32_t pc_temp = pc_value + (sign_extend_30(signed_immed_24) << 2);
+	arm_write_register(p, PC, pc_temp);
+	return 0;
 }
 
 int arm_coprocessor_others_swi(arm_core p, uint32_t ins) {
@@ -121,15 +134,16 @@ int arm_miscellaneous(arm_core p, uint32_t ins) {
 
 
 /* INTERN FUNCTIONS */
-
-static void Branch(arm_core p, uint32_t branch_addr) {
-	arm_write_register(p, PC, branch_addr);
-}
-
 static void Link(arm_core p){
 	//Sauvegarde de PC dans LR
 	arm_write_register(p, LR, arm_read_register(p, PC));
 }
+/*
+static void Branch(arm_core p, uint32_t branch_addr) {
+	arm_write_register(p, PC, branch_addr);
+}
+
+
 
 static void Exchange(arm_core p) {
 	//CPSR_T_Bit = 1 (CPSR_T_Bit = CPSR[5])
@@ -141,3 +155,4 @@ static uint32_t SignExtend(uint32_t val) {
 	if(val & 0x800000) return ~( (val^0xFFFFFF) + 1) + 1;
 	return val;
 }
+*/
