7f190b0848088886e9949ecd8adb3f7d56850850
Josquin IMBERT <josquin.imbert@etu.univ-grenoble-alpes.fr>
2021-01-12 10:06:38 +0100

    Correction de arm_branch_other.c

--- arm_branch_other.c
+++ arm_branch_other.c
@@ -27,40 +27,36 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 #define SUCCESS 0
-static void Link(arm_core p);
 
-uint32_t sign_extend_30(int32_t value) {
-	return asr(value << 8, 8);
-}
-/*
+static uint32_t sign_extend_30(int32_t value);
+static void Link(arm_core p);
+static void Exchange(arm_core p, uint8_t val);
 static void Branch(arm_core p, uint32_t branch_addr);
 
-static void Exchange(arm_core p);
-static uint32_t SignExtend(uint32_t val);
-*/
 int arm_branch(arm_core p, uint32_t ins) {
-/*	uint32_t branch_addr = 0x0;
+	uint32_t branch_addr = 0x0;
 	int defined = 0;
 	if(get_bits(ins, 27, 25) == 0x5) { //B, BL ou BLX
-		branch_addr = get_bits(ins, 23, 0);
 		defined = 1;
+		uint32_t pc_value = arm_read_register(p, PC);
+		uint32_t signed_immed_24 = get_bits(ins, 23, 0);
+		branch_addr = pc_value + (sign_extend_30(signed_immed_24) << 2);
 		if(get_bits(ins,31,28) == 0xF || get_bit(ins,24)) {
 			Link(p);
 		}
 		if(get_bits(ins,31,28) == 0xF) {
-			Exchange(p);
-			branch_addr = arm_read_register(p, PC) + (SignExtend(branch_addr) << 2) + (get_bit(ins,24) << 1);
+			Exchange(p, 1);
+			branch_addr += (get_bit(ins,24) << 1); //Adding H bit
 		}
 	} else if(get_bits(ins, 27, 6) == 0x04BFFC) { //BLX, BX, BXJ
 		uint8_t Rm = get_bits(ins, 3, 0);
 		uint8_t br = get_bits(ins, 5, 4);
 		defined = (br!=0) ? 1 : 0;
 		uint32_t Rm_value = arm_read_register(p, Rm);
-		uint32_t cpsr_value = arm_read_register(p, CPSR);
-		uint8_t J = get_bit(cpsr_value,24), E = get_bit(cpsr_value,9);
 		branch_addr = Rm_value & 0xFFFFFFFE;
-		if(br==3 || br==1 || (br==2 && ( (!J && !E)  ) ) ) {
-		Exchange(p);
+		if(br==3 || br==1 || br==2 ) {
+			//To simplify, we consider BXJ always exchanges the same way
+			Exchange(p, get_bit(Rm_value, 0));
 		}
 		if(br==3) {
 			Link(p);
@@ -70,15 +66,7 @@ int arm_branch(arm_core p, uint32_t ins) {
 		Branch(p, branch_addr);
 		return SUCCESS;
 	}
-*/  
-	uint32_t pc_value = arm_read_register(p, PC);
-	if(get_bit(ins, 24)){
-		arm_write_register(p, LR, pc_value-4);
-	}
-	uint32_t signed_immed_24 = get_bits(ins, 23, 0);
-	uint32_t pc_temp = pc_value + (sign_extend_30(signed_immed_24) << 2);
-	arm_write_register(p, PC, pc_temp);
-	return 0;
+	return UNDEFINED_INSTRUCTION;
 }
 
 int arm_coprocessor_others_swi(arm_core p, uint32_t ins) {
@@ -104,7 +92,7 @@ int arm_coprocessor_others_swi(arm_core p, uint32_t ins) {
 			//CHANGING PC
 			//I consider high vectors are not configured
 			uint32_t br_addr = 0x00000008; //Otherwise, we'd do : uint32_t br_addr = (get_bit(CP15_reg1, 13)) ? 0xFFFF0008 : 0x00000008;
-			arm_write_register(p, PC, br_addr);
+			Branch(p, br_addr);
 		}
 		return SOFTWARE_INTERRUPT;
 	}
@@ -134,25 +122,28 @@ int arm_miscellaneous(arm_core p, uint32_t ins) {
 
 
 /* INTERN FUNCTIONS */
-static void Link(arm_core p){
-	//Sauvegarde de PC dans LR
-	arm_write_register(p, LR, arm_read_register(p, PC));
+static uint32_t sign_extend_30(int32_t value) {
+	return asr(value << 8, 8);
 }
-/*
+
+/*Sauvegarde de PC dans LR*/
+static void Link(arm_core p){
+	arm_write_register(p, LR, arm_read_register(p, PC) - 4);
+}
+
+/*CPSR_T_Bit (CPSR[5]) = val*/
+static void Exchange(arm_core p, uint8_t val) { 
+	uint32_t cpsr_value;
+	if(val) cpsr_value = set_bit(arm_read_register(p, CPSR),5);
+	else cpsr_value = clr_bit(arm_read_register(p, CPSR),5);
+	arm_write_register(p, CPSR, cpsr_value);
+}
+
+/*Branchement à l'adresse branch_addr*/
 static void Branch(arm_core p, uint32_t branch_addr) {
 	arm_write_register(p, PC, branch_addr);
 }
 
 
 
-static void Exchange(arm_core p) {
-	//CPSR_T_Bit = 1 (CPSR_T_Bit = CPSR[5])
-	uint32_t cpsr_value = arm_read_register(p, CPSR) | (0x1 << 5);
-	arm_write_register(p, CPSR, cpsr_value);
-}
 
-static uint32_t SignExtend(uint32_t val) {
-	if(val & 0x800000) return ~( (val^0xFFFFFF) + 1) + 1;
-	return val;
-}
-*/
