40153d5a2c1396662008e66fd8cd01445ddeaa9d
Josquin IMBERT <josquin.imbert@etu.univ-grenoble-alpes.fr>
2021-01-13 21:00:17 +0100

    Ajout des commentaires au test 12

--- Examples/example12.s
+++ Examples/example12.s
@@ -3,9 +3,9 @@
 	B traitant_SWI
 .text
 main:
-	swi 0x000000
-	swi 0x123456
+	swi 0x000000 @devrait lancer le traitant traitant_SWI
+	swi 0x123456 @devrait mettre fin à l'exécution
 traitant_SWI:
-	mov r0, #5
-	movs PC,R14
+	mov r0, #5 @devrait mettre 5 dans r0 (pour s'assurer que le traitant s'exécute)
+	movs PC,R14 @devrait renvoyer dans le main
 .data
